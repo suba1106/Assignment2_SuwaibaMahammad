aae0e9524839bf538e5c1818c1a1b430dc20e9f4
suba1106 <Suwaiba116@gmail.com>
2025-05-07 11:43:10 -0500

    set up a overflow for connect based on the game code

--- src/main/java/Door.java
+++ src/main/java/Door.java
@@ -1,2 +1,16 @@
 public class Door {
+    Door door;
+
+    //connects 2 chambers by door
+    public static void connect(Chamber ch1, Chamber ch2){
+
+    }
+
+    public static void connect(Chamber ch1, Chamber ch2, Monster monster){
+
+    }
+
+    public Door getDoor() {
+        return door;
+    }
 }
