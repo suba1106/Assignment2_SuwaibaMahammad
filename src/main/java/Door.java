3df5a222051d2a2c6851fd154b79da08f1181fcd
suba1106 <Suwaiba116@gmail.com>
2025-05-10 13:45:40 -0500

    added the code to the connect methods

--- src/main/java/Door.java
+++ src/main/java/Door.java
@@ -1,17 +1,26 @@
 public class Door {
-    Door door;
+    private Chamber chamber1;
+    private Chamber chamber2;
+    private Monster monster;
 
     //connects 2 chambers by door
-    public static void connect(Chamber ch1, Chamber ch2){
-
+    public void connect(Chamber ch1, Chamber ch2){
+        this.chamber1 = ch1;
+        this.chamber2 = ch2;
+        ch1.addDoor(this);
+        ch2.addDoor(this);
     }
 
-    public static void connect(Chamber ch1, Chamber ch2, Monster monster){
-
+    public void connect(Chamber ch1, Chamber ch2, Monster monster){
+        this.chamber1 = ch1;
+        this.chamber2 = ch2;
+        this.monster = monster;
+        ch1.addDoor(this);
+        ch2.addDoor(this);
     }
 
     public Door getDoor() {
-        return door;
+        return this;
     }
 
     public int size() {
