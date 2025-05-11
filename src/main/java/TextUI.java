322c7d4f227a803b24e52fb6dfe091ea52937858
suba1106 <Suwaiba116@gmail.com>
2025-05-11 14:59:36 -0500

    added the monsters in the room

--- src/main/java/TextUI.java
+++ src/main/java/TextUI.java
@@ -9,33 +9,47 @@ public class TextUI {
             print(d);
             Action a = ask(d);
             a.execute();
+        }
     }
-}
 
-/** Print the current room of the dungeon. */
-private void print(Dungeon d) {
-    Chamber r = d.getCurrentChamber();
-    StringBuilder s = new StringBuilder();
-    s.append("You are in a chamber with " + r.getDoors().size() + " doors\n");
-    s.append("There are " + r.getItems().size() + " items in the chamber\n");
-    // TODO: print for each door which monster is there, how strong it is, how skilled in craft, and how healty
-    System.out.println(s.toString()); }
-/** Asks the user for an action. */
-private Action ask(Dungeon d) {
-    StringBuilder s = new StringBuilder();
-    s.append("Here are your options:\n");
-    List<Action> actions = d.getActions();
-    for (int i = 0; i < actions.size(); i++) {
-        Action a = actions.get(i); s.append("\t" + i + ": " + a.toString() + "\n");
+    /** Print the current room of the dungeon. */
+    private void print(Dungeon d) {
+        Chamber r = d.getCurrentChamber();
+        StringBuilder s = new StringBuilder();
+        s.append("You are in a chamber with " + r.getDoors().size() + " doors\n");
+        s.append("There are " + r.getItems().size() + " items in the chamber\n");
+        System.out.println("Here is the list of monsters in the room with you:");
+        List<Door> doors = r.getDoors();
+        for (int i = 0; i < doors.size(); i++) {
+            if (doors.get(i).hasMonster())
+                System.out.println(i + ": Door: " + doors.get(i).getMonster() + "\n  Strength: " +
+                        doors.get(i).getMonster().getStrength() + "\n  Craft: " +
+                        doors.get(i).getMonster().getCraft() + "\n  Health: " +
+                        doors.get(i).getMonster().getHealth());
+            else {
+                System.out.print(i + ": Door: has no monsters");
+            }
+        }
+        System.out.println(s.toString());
     }
-    System.out.println(s.toString());
-    // ask for action
-    BufferedReader reader = new BufferedReader( new InputStreamReader(System.in));
-    try {
-        int command = Integer.parseInt(reader.readLine());
-        return actions.get(command);
-    } catch (IOException e) {
-        return new PrintError(d, e);
+
+    /** Asks the user for an action. */
+    private Action ask(Dungeon d) {
+        StringBuilder s = new StringBuilder();
+        s.append("Here are your options:\n");
+        List<Action> actions = d.getActions();
+        for (int i = 0; i < actions.size(); i++) {
+            Action a = actions.get(i);
+            s.append("\t" + i + ": " + a.toString() + "\n");
+        }
+        System.out.println(s.toString());
+        // ask for action
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            int command = Integer.parseInt(reader.readLine());
+            return actions.get(command);
+        } catch (IOException e) {
+            return new PrintError(d, e);
+        }
     }
-}
 }
