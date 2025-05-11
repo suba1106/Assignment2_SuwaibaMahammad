4bc1c6e41ebac1c1710c337b3d25f0ff22487ff6
suba1106 <Suwaiba116@gmail.com>
2025-05-11 14:37:05 -0500

    fixed the second constructor

--- src/main/java/Fight.java
+++ src/main/java/Fight.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Random;
 
@@ -11,8 +14,26 @@ class Fight extends Action {
         this.monster = monster;
     }
 
-    public Fight(Character player, List<Monster> monster) {
-        // TODO Auto-generated constructor stub
+    public Fight(Character player, List<Monster> monster) { // if multiple monsters in chamber ask which
+        this(player, chooseMonster(monster));
+
+    }
+
+    private static Monster chooseMonster(List<Monster> monster) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            try {
+                System.out.println("Choose a monster:");
+                for (int i = 0; i < monster.size(); i++) {
+                    System.out.println(i + ": " + monster.get(i).getName());
+                }
+                int choice = Integer.parseInt(reader.readLine());
+                return monster.get(choice);
+            } catch (IOException e) {
+                System.out.println("Invalid input. Please try again.");
+            }
+        }
+
     }
 
     public void execute() {
@@ -41,6 +62,7 @@ class Fight extends Action {
             int playerTotal = playerRoll + playerStat + itemBonus;
             int monsterTotal = monsterRoll + monsterStat;
 
+            System.out.println("You rolled: " + playerRoll + ", Monster rolled: " + monsterRoll);
             System.out.println("Player total: " + playerTotal + ", Monster total: " + monsterTotal);
 
             if (playerTotal > monsterTotal) {
