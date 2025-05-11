a34b2f5bb7ea839197412b660b68b9685e369ff1
suba1106 <Suwaiba116@gmail.com>
2025-05-11 15:01:05 -0500

    removed duplicate methods

--- src/main/java/MagicWand.java
+++ src/main/java/MagicWand.java
@@ -2,14 +2,4 @@ class MagicWand extends Item {
     MagicWand() {
         super("Axe", 7, 1);
     }
-
-    @Override
-    protected int getStrength() {
-        return 1;
-    }
-
-    @Override
-    protected int getCraft() {
-        return 7;
-    }
 }
