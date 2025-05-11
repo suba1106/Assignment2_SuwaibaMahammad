5977e3a58e3b16509ab377b634881b748adc27b6
suba1106 <Suwaiba116@gmail.com>
2025-05-10 19:16:34 -0500

    fixed issues

--- src/main/java/Chamber.java
+++ src/main/java/Chamber.java
@@ -1,29 +1,44 @@
-import java.util.Collection;
 import java.util.List;
 import java.util.ArrayList;
 
 public class Chamber {
     Chamber chamber;
     List<Door> doors = new ArrayList<>();
-    List<Item> items = new ArrayList<>();
+    Item items;
 
     public Chamber() {
-
     }
 
     public Chamber(Item item) {
-        this.items.add(item);
+        this.items = item;
     }
 
     public List<Door> getDoors() {
         return doors;
     }
 
-    public List<Item> getItems() {
+    public Item getItems() {
         return items;
     }
 
-    private void addDoor(Door door) { //adds doors to the lost of doors in the chamber
+    public void addDoor(Door door) { // adds doors to the lost of doors in the chamber
         doors.add(door);
+    }
+
+    public boolean hasMonster() {
+        for (Door door : doors) {
+            if (door.getMonster() != null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Monster getMonster() {
+        for (Door door : doors) {
+            if (door.getMonster() != null) {
+                System.out.print("Monster Name: " + door.getMonster().name());
+            }
+        }
     }
 }
