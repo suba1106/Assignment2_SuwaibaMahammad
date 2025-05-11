3476d6fa065a4e0f470df5296c4616d560a88f36
suba1106 <Suwaiba116@gmail.com>
2025-05-11 17:51:39 -0500

    deleted useless methods

--- src/main/java/Axe.java
+++ src/main/java/Axe.java
@@ -2,15 +2,4 @@ class Axe extends Item {
     Axe() {
         super("Axe", 0, 8);
     }
-
-    @Override
-    protected int getStrength() {
-        return 8;
-    }
-
-    @Override
-    protected int getCraft() {
-        return 0;
-    }
-
 }
