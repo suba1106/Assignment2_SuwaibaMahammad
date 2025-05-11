09ced3f9448e557c9ea2515fafd518f914717b76
suba1106 <Suwaiba116@gmail.com>
2025-05-10 19:57:52 -0500

    implemented magicwand class

--- src/main/java/MagicWand.java
+++ src/main/java/MagicWand.java
@@ -1,6 +1,15 @@
-class MagicWand implements Item{
+class MagicWand extends Item {
+    MagicWand() {
+        super("Axe", 7, 1);
+    }
+
     @Override
-    public int size() {
-        return 0;
+    protected int getStrength() {
+        return 1;
+    }
+
+    @Override
+    protected int getCraft() {
+        return 7;
     }
 }
