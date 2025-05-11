edb798ab344ab07064bb4cd4c5f383418cf6b9e6
suba1106 <Suwaiba116@gmail.com>
2025-05-11 15:44:39 -0500

    made printerror extend action

--- src/main/java/PrintError.java
+++ src/main/java/PrintError.java
@@ -1,6 +1,15 @@
 import java.io.IOException;
 
-public class PrintError {
+public class PrintError extends Action {
+    Dungeon dungeon;
+    IOException e;
+
     public PrintError(Dungeon d, IOException e) {
+        this.dungeon = d;
+        this.e = e;
+    }
+
+    public void execute() {
+        System.err.println("Error reading input: " + e.getMessage());
     }
 }
