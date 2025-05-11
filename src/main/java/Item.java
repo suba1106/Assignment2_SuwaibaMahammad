1bc6d2743af46afb462bd0e2ab32d3ff4da4baae
suba1106 <Suwaiba116@gmail.com>
2025-05-10 22:12:36 -0500

    added getStrength adn getCraft

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,6 +1,19 @@
 public abstract class Item {
     String name = "";
+    int craft;
+    int strength;
 
     protected Item(String name, int craft, int strength) {
+        this.name = name;
+        this.craft = craft;
+        this.strength = strength;
+    }
+
+    protected int getStrength() {
+        return strength;
+    }
+
+    protected int getCraft() {
+        return craft;
     }
 }
