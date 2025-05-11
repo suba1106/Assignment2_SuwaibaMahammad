3294cb2e89de997a437d32212512bd661dc4f9c4
suba1106 <Suwaiba116@gmail.com>
2025-05-11 17:51:26 -0500

    added an ovverflow method to use

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public abstract class Character {
     int strength;
@@ -28,19 +29,32 @@ public abstract class Character {
         }
     }
 
+    public boolean use(Item item1) {
+        if (hand[0] == null) {
+            hand[0] = item1;
+            return true;
+        } else if (hand[1] == null) {
+            hand[1] = item1;
+            return true;
+        } else {
+            System.out.println("You can only have 2 items in hand");
+            return false;
+        }
+    }
+
     // shows the inventory
     public void showInventory() {
         System.out.println("Inventory:");
-        for (Item item : inventory) {
-            System.out.println("- " + item.name);
+        for (int i = 0; i < inventory.size(); i++) {
+            System.out.println(i + ": " + inventory.get(i).name);
         }
     }
 
     // shows the equipped
     public void showEquipped() {
         System.out.println("Equipped:");
-        for (Item item : hand) {
-            System.out.println(item + " /n");
+        for (int i = 0; i < hand.length; i++) {
+            System.out.println(i + ": " + hand[i].name);
         }
     }
 
@@ -67,5 +81,22 @@ public abstract class Character {
     protected void takeDamage(int damage) {
         health -= damage;
     }
+
+    public List<Item> getInventory() {
+        return inventory;
+    }
+
+    public boolean swap(Item inventoryItem, Item handItem) {
+        for (int i = 0; i < equipped.length; i++) {
+            if (equipped[i] == inventoryItem) {
+                equipped[i] = handItem;
+                return true;
+        }
+    }return false;
+    }
+
+    // Item not found in equipped list
+    return false;
+}
 
 }
