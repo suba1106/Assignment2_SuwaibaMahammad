04e52907e2b7b65ca84afd712fe1c6e03d1c726f
suba1106 <Suwaiba116@gmail.com>
2025-05-10 19:15:05 -0500

    created blueprint for monster

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -1,5 +1,20 @@
 class Monster extends Character {
-    public Monster(String name, int str, int crf, int hea){
-        super(name,str,crf,hea);
+    public Monster(String name, int str, int crf, int hea) {
+        super(name, str, crf, hea);
+    }
+
+    public boolean isAlive() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isAlive'");
+    }
+
+    public String getName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getName'");
+    }
+
+    public boolean isDefeated() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isDefeated'");
     }
 }
