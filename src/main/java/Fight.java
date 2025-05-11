8248393b055e1ab189b1fc4fe2daf584bc8fdd89
suba1106 <Suwaiba116@gmail.com>
2025-05-10 19:24:33 -0500

    created constructor

--- src/main/java/Fight.java
+++ src/main/java/Fight.java
@@ -1,6 +1,12 @@
+import java.util.List;
+
 class Fight extends Action {
 
     public Fight(Character player, Monster monster) {
+        // TODO Auto-generated constructor stub
+    }
+
+    public Fight(Character player, List<Monster> monster) {
         // TODO Auto-generated constructor stub
     }
 }
