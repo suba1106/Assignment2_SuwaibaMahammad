8e401d00e944ffd1ecd87b87bb3e755ffaa3f5c8
suba1106 <Suwaiba116@gmail.com>
2025-05-12 13:08:36 -0500

    added a boss fight to the game

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -5,6 +5,7 @@ public class Game {
                 new Chamber(),
                 new Chamber(new Axe()),
                 new Chamber(new Shield()),
+                new Chamber(new MagicWand()),
                 new Chamber(), new Chamber()
         };
         // connect the chambers with doors
@@ -12,6 +13,7 @@ public class Game {
         Door.connect(chambers[1], chambers[2], new Monster("Goblin", 1, 0, 3));
         Door.connect(chambers[2], chambers[3], new Monster("Spider", 3, 0, 5));
         Door.connect(chambers[3], chambers[4]);
+        Door.connect(chambers[4], chambers[5], new Monster("Boss", 0, 6, 7));
         Character player = new Wizard("Gandalf");
         // initialize the dungeon with player, entry chamber, and goal chamber
         Dungeon d = new Dungeon(player, chambers[0], chambers[4]);
